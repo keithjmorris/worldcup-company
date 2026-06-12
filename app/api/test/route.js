2e3ebd33173dc6a3838b125de3aa3d03303f5932
keithjmorris <keith.morris@outlook.com>
2026-06-12 17:13:34 +0100

    More testing

--- app/api/test/route.js
+++ app/api/test/route.js
@@ -2,5 +2,7 @@ export async function GET() {
   return Response.json({
     hasFirebaseKey: !!process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     keyStart: process.env.NEXT_PUBLIC_FIREBASE_API_KEY?.substring(0, 5),
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    appUrl: process.env.NEXT_PUBLIC_APP_URL,
   });
 }
